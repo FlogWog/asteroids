3da0eee37547f2ebebabe3a10e18cd36524ec76a
FlogWog <flogsworth@gmail.com>
2025-04-14 16:10:38 -0700

    rotating triangle

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
+from player import Player
     
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # below is the game loop
     while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
-                  retun
-        screen.fill((0, 0, 0))
+                return
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
